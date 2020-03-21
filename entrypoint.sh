83ca32c2f807fecb668ca191730991ee82bd8f81
Olaf Flebbe <of@oflebbe.de>
2020-03-21 17:04:59 +0100

    deleting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 if [ "x$INPUT_EXCLUDEDIR" != x ] ; then
-   EXCLUDE="-X $INPUT_EXCLUDEDIR"
+   EXCLUDE="-x $INPUT_EXCLUDEDIR"
 fi
 
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror -v --delete --reverse --continue --dereference -X '.git' $EXCLUDE $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror -v --delete --reverse --ignore-time --dereference -x '/\.git.*' $EXCLUDE $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
