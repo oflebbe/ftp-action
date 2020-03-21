cff4b7000f3335abb0a1dc6cec45740a55cd29f4
Olaf Flebbe <of@oflebbe.de>
2020-03-21 17:12:51 +0100

    regex correct 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ "x$INPUT_EXCLUDEDIR" != x ] ; then
    EXCLUDE="-x $INPUT_EXCLUDEDIR"
 fi
 
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror -v --delete --reverse --ignore-time --dereference -x ^\.git/$  $EXCLUDE $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror -v --delete --reverse --ignore-time --dereference -x ^\.git.*/$  $EXCLUDE $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
