38c439438dc38308ba8878b57a1f22d8d059cc6d
Olaf Flebbe <of@oflebbe.de>
2020-03-21 16:36:31 +0100

    next try

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 if [ "x$INPUT_EXCLUDEDIR" != x ] ; then
    EXCLUDE="-x $INPUT_EXCLUDEDIR"
 fi
+echo x$INPUT_HOST x$EXCLUDE xxxx
 lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ssl:verify-certificate false; mirror --reverse --continue --dereference -x '.git' $EXCLUDE $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
